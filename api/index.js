4989f8b9caf9248e6a7cce8bb569629ff28f7938
youssef Nasser <kakahoba012@gmail.com>
2026-08-09 04:31:17 +0300

    solve problems

---
+++ api/index.js
@@ -0,0 +1,22 @@
+// api/index.js
+const { NestFactory } = require('@nestjs/core');
+const { ExpressAdapter } = require('@nestjs/platform-express');
+const express = require('express');
+const { AppModule } = require('../dist/app.module');
+
+const server = express();
+let cachedApp;
+
+async function bootstrap() {
+  if (!cachedApp) {
+    const app = await NestFactory.create(AppModule, new ExpressAdapter(server));
+    await app.init();
+    cachedApp = server;
+  }
+  return cachedApp;
+}
+
+module.exports = async (req, res) => {
+  const app = await bootstrap();
+  app(req, res);
+};
