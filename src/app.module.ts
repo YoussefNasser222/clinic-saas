996c7a3f4f7642b62ad17407ac52c7c3391ff116
youssef Nasser <kakahoba012@gmail.com>
2026-08-11 03:06:02 +0300

    solve problem

--- src/app.module.ts
+++ src/app.module.ts
@@ -2,7 +2,7 @@ import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { ConfigModule, ConfigService } from '@nestjs/config';
-import devConfig from '@config/env/dev.config';
+import devConfig from "./config/env/dev.config";
 import { MongooseModule } from '@nestjs/mongoose';
 import { AuthModule } from './modules/auth/auth.module';
 import { DoctorModule } from './modules/doctor/doctor.module';
